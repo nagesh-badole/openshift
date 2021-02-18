f95c1bb789321993e1a255982a1305ee857d5eda
nagesh-badole <35701917+nagesh-badole@users.noreply.github.com>
2021-02-18 16:25:52 +0530

    Delete jupyterhub_config.py

--- jupyterhub_config.py
+++
@@ -1,6 +0,0 @@
-c.Spawner.args = [ '--NotebookApp.tornado_settings={"headers": {"Content-Security-Policy": "frame-ancestors * self "}}']
-c.JupyterHub.tornado_settings = {'headers': {'Content-Security-Policy': "frame-ancestors * 'self' "}}
-c.GatewayClient.request_timeout = 600.0
-c.JupyterApp.log_levelany = 50
-c.Application.log_levelany = 50
-c.Spawner.default_url = '/lab'
